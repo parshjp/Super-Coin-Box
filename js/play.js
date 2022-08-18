862bdbe127f3383ac6795def5427f95dc479d594
parshjp <pjparikh1023@gmail.com>
2022-08-17 21:22:39 -0400

    added improvements, no functionality changes

--- js/play.js
+++ js/play.js
@@ -10,10 +10,37 @@ class Play{
         this.scoreLabel = this.add.text(30,25, 'Score: 0', {fontSize: '18px Arial', fill: '#fff'});
         this.score = 0;
         this.enemies = this.physics.add.group();
-        this.time.addEvent({
-            delay: 2200,
-            callback: ()=> this.addEnemy(),
-            loop: true
+        this.nextEnemy = 0; //next enemy creation time
+
+
+        this.jumpSound = this.sound.add('jump');
+        this.coinSound = this.sound.add('coin');
+        this.deadSound = this.sound.add('dead');
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player', {frames: [1, 2]}),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player', {frames: [3, 4]}),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        //create particles
+        let particles = this.add.particles('pixel');
+
+        //create emitter
+        this.emitter = particles.createEmitter({
+            quantity: 15,
+            speed: {min : -150, max: 150},  //between -150 and 150 pixels per second
+            scale: {start: 2, end: 0}, //twice the size and end at 0
+            lifespan: 800, 
+            on: false //don't start the explosion right away
         });
     }
 
@@ -21,6 +48,12 @@ class Play{
     update(){
         this.physics.collide(this.player, this.walls);
         this.physics.collide(this.enemies, this.walls);
+
+        //if the player dies then nothing
+        if(!this.player.active){
+            return;
+        }
+
         if (!this.player.active){
             return;
         }
@@ -34,21 +67,46 @@ class Play{
         if(this.physics.overlap(this.player, this.enemies)){
             this.playerDie();
         }
+
+        let now = Date.now(); //current time in milliseconds, create more enemies over time
+
+        if(this.nextEnemy < now){
+            let startDiff = 4000;
+            let endDiff = 1000;
+            let scoreEndDiff = 100;
+
+            //compute progress, which is 0<=progress<=1
+            //0 is start
+            //1 is end score
+            let progress= Math.min(this.score/scoreEndDiff, 1);
+
+            //compute the time between enemies
+            let delay = startDiff - (startDiff - endDiff) * progress;
+
+            this.addEnemy();
+            this.nextEnemy = now + delay;
+        }
+
+
     }
 
     //handle player movement
     movePlayer(){
         if(this.arrow.left.isDown){
             this.player.body.velocity.x = -200;
+            this.player.anims.play('left', true);
         }
         else if(this.arrow.right.isDown){
             this.player.body.velocity.x = 200;
+            this.player.anims.play('right', true);
         }
         else{
             this.player.body.velocity.x = 0;
+            this.player.setFrame(0);
         }
         if(this.arrow.up.isDown && this.player.body.onFloor()){
             this.player.body.velocity.y = -320;
+            this.jumpSound.play();
         }
     }
 
@@ -71,9 +129,28 @@ class Play{
     }
 
     takeCoin(){
+
+        //scale the coin to 0 so it is invisible
+        this.coin.setScale(0);
+
+        this.tweens.add({
+            targets: this.coin,
+            scale: 1,   //scale to its original size
+            duration: 300, //300ms
+        });
+
+        //players gets bigger when they collect a coin
+        this.tweens.add({
+            targets: this.player,
+            scale: 1.3, 
+            duration: 100,
+            yoyo: true, //reverse to original size
+        });
+
         this.score += 5;
         this.scoreLabel.setText('Score: ' + this.score);
         this.updateCoinPosition();
+        this.coinSound.play();
     }
 
     updateCoinPosition(){
@@ -104,6 +181,21 @@ class Play{
     }
 
     playerDie(){
-        this.scene.start('menu', {score: this.score});
+
+        this.cameras.main.shake(300, 0.03);
+
+        this.player.destroy();
+
+        //go to menu when player dies
+        this.deadSound.play();
+
+        //set emitter on top of player
+        this.emitter.setPosition(this.player.x, this.player.y);
+        this.emitter.explode();
+
+        this.time.addEvent({
+            delay: 1000,
+            callback: ()=> this.scene.start('menu', {score: this.score}),
+        });
     }
 };
